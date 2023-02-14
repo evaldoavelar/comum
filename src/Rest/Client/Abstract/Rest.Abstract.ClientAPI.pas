a220f6ad29f597d156cbaa648fa3dad39acb2413
Evaldo Avelar Marques <evaldoavelar@gmail.com>
2023-02-14 14:45:30 -0300

    Update Rest.Abstract.ClientAPI.pas

--- src/Rest/Client/Abstract/Rest.Abstract.ClientAPI.pas
+++ src/Rest/Client/Abstract/Rest.Abstract.ClientAPI.pas
@@ -1,5 +1,6 @@
 unit Rest.Abstract.ClientAPI;
 
+
 interface
 
 uses
@@ -12,17 +13,19 @@ type
     function Accept(aAccept: string): IRestClientAPI;
     function AcceptCharset(aAcceptCharset: string): IRestClientAPI;
     function ContentType(aContentType: string): IRestClientAPI;
+    function TokenBearer(aToken: string): IRestClientAPI;
 
     function Method(aMethod: TRESTRequestMethod): IRestClientAPI;
     function Resource(aResource: string): IRestClientAPI;
-    function AddParameterBody(const AName, aValue: string): IRestClientAPI;
+    function AddParam_x_www_form_urlencoded(const AName, aValue: string): IRestClientAPI;
     function Params: TRESTRequestParameterList;
-    function Body:TCustomRESTRequest.Tbody;
+    function Body: TCustomRESTRequest.Tbody;
     function Execute: TCustomRESTResponse;
 
     function UserName(aUserName: string): IRestClientAPI;
     function Password(aPassword: string): IRestClientAPI;
     function TimeOut(aTimeOut: integer): IRestClientAPI;
+
   end;
 
 implementation
